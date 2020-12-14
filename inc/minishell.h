929e9245cded2857a71f1598c48aed657b3fb60e
jmaydew <jmaydew@student.42.fr>
2020-12-14 14:02:08 +0100

    Code structure: parsing call in main, then depending on parsing ret, call func to execute built-in cmd or local cmd. Macros for BUILT_IN_CMD and LOCAL_CMD added in minishell.h
    FEAT:
    1. Parsing - perform a split and check return value for errors
    2. Split_free function - called on every loop
    3. Free after strjoin
    4. Wait in parent process until child is complete

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,13 +3,18 @@
 
 # include <stdio.h>
 # include <sys/types.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 # include <unistd.h>
 # include <errno.h>
 # include <string.h>
 # include <limits.h>
+
 # include "get_next_line.h"
 # include "libft.h"
 
-# define BIN_PATH "/bin/"
+# define BIN_PATH       "/bin/"
+# define BUILT_IN_CMD   1
+# define LOCAL_CMD      2
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,42 +1,92 @@
 #include "minishell.h"
 
 int
-	main(void)
+	execute_builtin(char **arg_split)
 {
 	pid_t	child;
-	char	*line;
-	int		gnl_ret;
-	char	**input_split;
+	int		wstatus; //passed to wait
 	char	*file_path;
+	
+	file_path = NULL;
+	if ((child = fork()) < 0)
+		fprintf(stderr, "%s\n", strerror(errno));
+	else if (child == 0)
+	{
+		printf("Child: \n"); //For Debug purposes
+		if ((file_path = ft_strjoin(BIN_PATH, arg_split[0])) == NULL)
+			return (printf("Join error\n")); //error_free
+		execve(file_path, arg_split, NULL);
+		fprintf(stderr, "%s\n", strerror(errno));
+	}
+	else
+	{
+		printf("Pid: %d\n", wait(&wstatus));
+		printf("Goodbye from parent!\n");
+		free(file_path);
+	}
+	return (0);
+}
+
+int
+	parse_input(char *line, char ***line_split)
+{
+
+	if (!(*line_split = ft_split(line, ' '))) //Clement: use macro for ' '?
+	{
+		return (-1);
+	}
+	if (*line_split == NULL)	//Empty line, but may be useless considering we don't read unless we receive a \n from stdin??
+	{
+		printf("Empty line");
+		return (0);
+	}
+	// + Check len and max arg here!
+	// + Check if 1st arg of split is NULL
+	/*
+	** Check 1st arg:
+	**	if built-in cmd: return BUILT_IN_CMD
+	**	if local cmd (one we have to manage ourselves): return LOCAL_CMD
+	**	else: set errno; return -1
+	*/
+	return (BUILT_IN_CMD);
+}
+
+void
+	free_split(char ***line_split)
+{
+	int	i;
+
+	i = 0;
+	while ((*line_split)[i])
+	{
+		free((*line_split)[i]);
+		i++;
+	}
+	free(*line_split);
+}
+
+int
+	main(void)
+{
+	char	*line;
+	char	**line_split;
+	int		gnl_ret;
+	int		split_ret;
 
 	while (1)
 	{
 		write(STDIN_FILENO, "Coquillage $>", 13);
-		//Need considerations for MAX_ARGS and MAX_ARG_STRLEN
 		if ((gnl_ret = get_next_line(STDIN_FILENO, &line)) < 0)
-			fprintf(STDERR_FILENO, "%s\n", strerror(errno));		
+			fprintf(stderr, "%s\n", strerror(errno));		
 		else
-		{	
-			if ((child = fork()) < 0)
-			{
-				fprintf(STDERR_FILENO, "%s\n", strerror(errno));
-			}
-			else if (child == 0)
-			{
-				printf("Child: \n");
-				if (!(input_split = ft_split(line, ' ')))
-					return(printf("Split error\n")); //error_free
-				if (*input_split == NULL)
-					return (printf("Empty line\n")); //error_free
-				if ((file_path = ft_strjoin(BIN_PATH, input_split[0])) == NULL)
-					return (printf("Join error\n")); //error_free
-				execve(file_path, input_split, NULL);
-				printf("%s\n", strerror(errno));
-			}
-			else
-				printf("Hello from parent!\n");
-			free(line);
+		{
+			if ((split_ret = parse_input(line, &line_split)) < 0)
+				fprintf(stderr, "%s\n", strerror(errno));
+			else if (split_ret == BUILT_IN_CMD)
+				execute_builtin(line_split);
 		}
+		free(line);
+		free_split(&line_split);
 	}	
 	return (0);
 }
