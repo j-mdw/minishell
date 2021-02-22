a9dc8fed17fb6e943ad980814474cf684289013d
PiloucK <clkuznie@student.42.fr>
2021-02-22 15:14:53 +0100

    general norm organisation + empty arg case and complete empty params (line full of non-existing env variables)

--- src/param_trim.c
+++ src/param_trim.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 14:42:27 by user42            #+#    #+#             */
-/*   Updated: 2021/02/20 16:58:41 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/22 14:46:02 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,30 @@
 
 typedef struct	s_exp_utils
 {
-	int		total_len;
-	int		char_is_lit;
-	char	quote_type;
+	t_lit_status	lit_status;
+	t_list			*local_env;
+	char			*string_to_free;
+	char 			*raw_param;
+	char			*final_param;
+	int				char_is_lit;
+	char			quote_type;
 }				t_exp_utils;
 
 
 static int
-	dollar_expansion(char **raw_param, char **value_str, t_list *local_env)
+	dollar_expansion(t_exp_utils *utils, char **value_str)
 {
 	char		*sub_param;
 	int			i;
 
 	i = 0;
-	sub_param = (*raw_param)++;
-	while (ft_isalnum(**raw_param) || **raw_param == '_')
-		(*raw_param)++;
-	if (*raw_param != sub_param + 1)
+	sub_param = (utils->raw_param)++;
+	while (ft_isalnum(*utils->raw_param) || *utils->raw_param == '_')
+		(utils->raw_param)++;
+	if (utils->raw_param != sub_param + 1)
 	{
-		sub_param = ft_strndup(sub_param + 1, *raw_param - sub_param);
-		*value_str = env_get_val(local_env, sub_param);
+		sub_param = ft_strndup(sub_param + 1, utils->raw_param - sub_param);
+		*value_str = env_get_val(utils->local_env, sub_param);
 		free(sub_param);
 	}
 	else
@@ -44,51 +48,61 @@ static int
 }
 
 static char		*
-	param_expand(char *raw_param, char *final_param, int total_len, t_list *local_env)
+	part_write(t_exp_utils *utils, char *value_str, int i)
+{
+	while (i-- > 0)
+	{
+		utils->final_param--;
+		utils->raw_param--;
+		if (!value_str)
+			*utils->final_param = *utils->raw_param;
+		else
+			*utils->final_param = value_str[i];
+	}
+	return (utils->final_param);
+}
+
+static char		*
+	param_expand(t_exp_utils utils, int total_len)
 {
 	int			i;
 	char		*value_str;
 
 	i = 0;
 	value_str = NULL;
-	if (*raw_param == '$')
-		i = dollar_expansion(&raw_param, &value_str, local_env);
+	if (*utils.raw_param == '$')
+		i = dollar_expansion(&utils, &value_str);
 	else
 	{
-		while (*raw_param && *raw_param != '$' && ++i)
-			raw_param++;
+		while (*utils.raw_param && *utils.raw_param != '$' && ++i)
+			utils.raw_param++;
 	}
-	if (*raw_param)
-		final_param = param_expand(raw_param, final_param, total_len + i, local_env);
+	if (*utils.raw_param)
+		utils.final_param = param_expand(utils, total_len + i);
 	else
 	{
-		if (!(final_param = malloc(total_len + i + 1)))
-			printf("Malloc error\n");
-		final_param[total_len + i] = '\0';
-		final_param = &final_param[total_len + i];
+		if (!(utils.final_param = malloc(total_len + i + 1)))
+			return (NULL); // Error on malloc, how to handle ??
+		utils.final_param[total_len + i] = '\0';
+		utils.final_param = &utils.final_param[total_len + i];
 	}
-	while (i-- > 0)
-	{
-		final_param--;
-		raw_param--;
-		if (!value_str)
-			*final_param = *raw_param;
-		else
-			*final_param = value_str[i];
-	}
-	return (final_param);
+	return (part_write(&utils, value_str, i));
 }
 
 char		*
 	param_trim(char *raw_param, t_list *local_env)
 {
 	char		*final_format;
+	t_exp_utils	utils;
 
 	if (!raw_param)
 		return (NULL);
-	if (raw_param[0])
-		final_format = param_expand(raw_param, NULL, 0, local_env);
-	else
-		final_format = ft_strdup(raw_param);
+	lit_status_init(&(utils.lit_status));
+	utils.raw_param = raw_param;
+	utils.char_is_lit = 0;
+	utils.final_param = NULL;
+	utils.local_env = local_env;
+	utils.string_to_free = NULL;
+	final_format = param_expand(utils, 0);
 	return (final_format);
 }
--- src/parse_argv.c
+++ src/parse_argv.c
@@ -6,11 +6,34 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 15:42:49 by user42            #+#    #+#             */
-/*   Updated: 2021/02/19 14:33:42 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/22 15:13:30 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char
+	**remove_empty_param(char **params)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (params[i])
+	{
+		if (!params[i][0])
+		{
+			j = i;
+			free(params[i]);
+			while (params[j++])
+				params[j - 1] = params[j];
+		}
+		i++;
+	}
+	// if (params[0]) TODO see if anything remains
+		return (params);
+	// return (NULL);
+}
 
 char
 	**parse_argv(char *cmd_line, t_list *local_env)
@@ -20,6 +43,7 @@ char
 	int		i;
 
 // check if the programm leaks if shell_split returns a pointer to a NULL pointer
+// DO THE EMPTY EXPANSION SHIT
 	params = shell_split(cmd_line, ' ');
 	i = 0;
 	while (params[i])
@@ -36,9 +60,9 @@ char
 			ft_free_strnarr(params, i);
 			free(params);
 			params = NULL;
-			break ;
+			return (params);
 		}
 		i++;
 	}
-	return (params);
+	return (remove_empty_param(params));
 }
