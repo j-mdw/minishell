9f2bb1926c5c2b970b3a8901663b8931e3fef1c9
user42 <clkuznie@student.42.fr>
2021-02-27 23:19:08 +0100

    $ before quoting, $ not printed

--- src/parse_param_trim.c
+++ src/parse_param_trim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_param_trim.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clkuznie <clkuznie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 14:42:27 by user42            #+#    #+#             */
-/*   Updated: 2021/02/26 14:23:02 by clkuznie         ###   ########.fr       */
+/*   Updated: 2021/02/27 23:17:05 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ static int
 		*value_str = env_get_val(utils->local_env, subparam);
 		free(subparam);
 	}
-	else if (utils->char_is_lit || *utils->raw_param != '?')
+	else if (utils->char_is_lit || (*utils->raw_param != '?'
+		&& *utils->raw_param != '\'' && *utils->raw_param != '"'))
 		*value_str = "$";
-	else
-	{
-		*value_str = utils->exit_status;
+	else if (*utils->raw_param != '?')
+		*value_str = "";
+	else if ((*value_str = utils->exit_status))
 		utils->raw_param++;
-	}
 	if (*value_str)
 		i = ft_strlen(*value_str);
 	return (i);
